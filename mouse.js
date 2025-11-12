e385862897beb55c3fdf11ed1ab2cfc6032f8f72
JoseBeltran123 <josebelt407@gmail.com>
2025-11-12 11:37:30 -0700

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -8,3 +8,34 @@ elementos.forEach(elemento => {
         elemento.style.transformOrigin = `${x}% ${y}%`;
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const carga = document.getElementById("onload");
+    var op = 1;
+    window.setInterval(function(){
+      carga.style.opacity = op;
+      if(carga.style.opacity <= "0"){
+        carga.style.visibility = "hidden";
+      }
+      if(carga.style.opacity > 0){
+        op= op -0.1;
+      }
+    },80);
+  const botonConfirmar = document.querySelector(".boton-confirm");
+  const ventanaCookies = document.querySelector(".header-principal");
+ 
+  
+  const aceptoCookies = localStorage.getItem("cookiesAceptadas");
+ 
+  if (aceptoCookies === "false") {
+    ventanaCookies.style.display = "none";
+  }
+ 
+  botonConfirmar.addEventListener("click", (e) => {
+ 
+    e.preventDefault(); 
+    ventanaCookies.style.display = "none"; 
+    localStorage.setItem("cookiesAceptadas", "true"); 
+  });
+ 
+});
